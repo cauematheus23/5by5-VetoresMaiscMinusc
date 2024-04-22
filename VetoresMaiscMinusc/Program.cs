eedc13e8011e3db56ab05552fed781441362b43a
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-22 14:36:40 -0300

    Add project files.

---
+++ VetoresMaiscMinusc/Program.cs
@@ -0,0 +1,35 @@
+﻿char[] carac = new char[10];
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Digite o {i+1}° caracter");
+    carac[i] = char.Parse( Console.ReadLine() );
+}
+for (int j = 0; j < 10; j++)
+{
+    if (carac[j] == 'a' || carac[j] == 'e' || carac[j] == 'i' || carac[j] == 'o' || carac[j] == 'u')
+    {
+        if (carac[j] == 'a')
+        {
+            carac[j] = 'A';
+        }
+        if (carac[j] == 'e')
+        {
+            carac[j] = 'E';
+        }
+        if (carac[j] == 'i')
+        {
+            carac[j] = 'I';
+        }
+        if (carac[j] == 'o')
+        {
+            carac[j] = 'O';
+        }
+        if (carac[j] == 'u')
+        {
+            carac[j] = 'U';
+        }
+
+    }
+    
+}
+Console.WriteLine(carac);
